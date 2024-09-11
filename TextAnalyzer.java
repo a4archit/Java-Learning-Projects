1f54d1b5bd3efbe3c0ca6a9cb0c1ac6eecd653a3
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-11 09:07:00 +0530

    Update TextAnalyzer.java
    Adding 'largestWord()' method

--- TextAnalyzer.java
+++ TextAnalyzer.java
@@ -8,15 +8,31 @@ public class TextAnalyzer {
         System.out.print("Enter some text: ");
         String s = sc.nextLine();
         
-        int words = getWords(s);
+        int words = lengthOfText(s);
         int noOfSentences = noOfSentences(s);
+        String lWord = largestWord(s);
+        
         //displying the output
         System.out.println("\n\tSome Insights: \n\t--------------\n");
         System.out.println("\tTotal words: " + words);
-        System.out.println("\tTotal Sentences: "+noOfSentences+"\n\n");
+        System.out.println("\tTotal Sentences: "+noOfSentences+"\n");
+        System.out.println("\tLargest Word: "+lWord+"\n");
+        
     }
     
-    public static int getWords(String text){
+    public static String getLargestWord(String text){
+        """ This method will return the largest word from given text """
+        String words[] = text.trim().split(" ");
+        String largestWord = words[0];
+        for (String word: words){
+            if (largestWord.length() < word.length()){
+                largestWord = word;
+            }
+        }
+        return largestWord;
+    }
+    
+    public static int lengthOfText(String text){
         int words;
         words = text.trim().split(" ").length;
         return words;
