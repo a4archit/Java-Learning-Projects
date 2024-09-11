627a50610736e8a6e0242b6af91179e34dfd32d2
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-11 13:00:59 +0530

    Update TextAnalyzer.java
    adding 'getSmallestWord()' method and improving some corrections

--- TextAnalyzer.java
+++ TextAnalyzer.java
@@ -11,17 +11,29 @@ public class TextAnalyzer {
         int words = lengthOfText(s);
         int noOfSentences = noOfSentences(s);
         String lWord = getLargestWord(s);
+        String sWord = getSmallWord(s);
         
         //displying the output
         System.out.println("\n\tSome Insights: \n\t--------------\n");
-        System.out.println("\tTotal words: " + words);
+        System.out.println("\tTotal words    : " + words);
         System.out.println("\tTotal Sentences: "+noOfSentences+"\n");
-        System.out.println("\tLargest Word: "+lWord+"\n");
+        System.out.println("\tLargest word   : "+lWord);
+        System.out.println("\tSmallest word  : "+sWord);
         
     }
-    
     public static String getLargestWord(String text){
-        """ This method will return the largest word from given text """
+    	// This method will return the smallest word from given text 
+        String words[] = text.trim().split(" ");
+        String smallestWord = words[0];
+        for (String word: words){
+            if (smallestWord.length() < word.length()){
+                smallestWord = word;
+            }
+        }
+        return smallestWord;
+    }
+    public static String getLargestWord(String text){
+        // This method will return the largest word from given text 
         String words[] = text.trim().split(" ");
         String largestWord = words[0];
         for (String word: words){
