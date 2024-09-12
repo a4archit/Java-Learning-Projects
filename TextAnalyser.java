6d2d3daf7d9284de7299b6038c9ba3b1b1d38266
Archit <115338571+a4archit@users.noreply.github.com>
2024-09-12 17:26:03 +0530

    Update TextAnalyser.java
    Improving some techniques and adding two functions:
    1. getTotalVowels() -> returns total number of vowels in text
    2. getVowelCounts() -> returns vowels  with their volume

--- TextAnalyser.java
+++ TextAnalyser.java
@@ -12,15 +12,79 @@ class TextAnalyser {
         int noOfSentences = noOfSentences(s);
         String lWord = getLargestWord(s);
         String sWord = getSmallestWord(s);
+        char vowels[] = "aeiou".toCharArray();
+        int totalVowels = getTotalVowels(s);
+        int vowelCounts[] = getVowelCounts(s);
+        
         
         //displying the output
         System.out.println("\n\tSome Insights: \n\t--------------\n");
         System.out.println("\tTotal words    : " + words);
-        System.out.println("\tTotal Sentences: "+noOfSentences+"\n");
+        System.out.println("\tTotal Sentences: "+noOfSentences);
         System.out.println("\tLargest word   : "+lWord);
         System.out.println("\tSmallest word  : "+sWord);
+        System.out.println("\tVowel insights : ");
+        System.out.println("\t\tTotal  : "+totalVowels);
+        for(int i=0; i<vowels.length; i++){
+            System.out.println("\t\t"+vowels[i]+"-> "+vowelCounts[i]);
+        }
         
     }
+    
+	public static int getTotalVowels(String text){
+        // Returns the total number of vowels(int)
+        int total = 0;
+        // getting vowel insights
+        int vowelArr[] = getVowelCounts(text);
+        for (int value: vowelArr){
+            total += value;
+        }
+        return total;
+    }
+    
+    public static int[] getVowelCounts(String text){
+        /********************************************
+         * Returns an array of length 5
+         * Each index of returnable array will in this format
+         *					a -> arr[0]
+         *  e -> arr[1]
+         *  i -> arr[2]
+         *  o -> arr[3]
+         *  u -> arr[4]
+         *******************************************/
+        
+        int vowelArr[] = {0,0,0,0,0};
+        char[] vowels, textArr;
+        
+        vowels = "aeiou".toCharArray();
+        textArr = text.toCharArray();
+        
+        for(char character: textArr){
+            for(char vowel: vowels){
+                if(character == vowel){
+                    switch(vowel){
+                        case 'a':
+                            vowelArr[0]++;
+                            break;
+                        case 'e':
+                            vowelArr[1]++;
+                            break;
+                        case 'i':
+                            vowelArr[2]++;
+                            break;
+                        case 'o':
+                            vowelArr[3]++;
+                            break;
+                        default:
+                            vowelArr[4]++;
+                    }
+                }
+            }
+        }
+        
+        return vowelArr;
+    }
+    
     public static String getSmallestWord(String text){
     	// This method will return the smallest word from given text 
         String words[] = text.trim().split(" ");
