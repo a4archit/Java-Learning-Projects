8d97293438e911847f096de02c4b4bcc760f3f9e
Archit <115338571+a4archit@users.noreply.github.com>
2024-10-19 21:59:03 +0530

    Update code basic structure

--- TextAnalyzer/VowelsMethods.java
+++ TextAnalyzer/VowelsMethods.java
@@ -1,1 +1,14 @@
 
+// package defintion
+package TextAnalyzer;
+
+// main class for this file
+class VowelsMethods{
+    // main method
+    public static void main(String[] args){
+
+    } // End of main method //
+    
+// ------------------------------------------------------- Methods definition starts form here -------------------------------------------------
+
+} // End of class 'ConsonantsMethods' //
